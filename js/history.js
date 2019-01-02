9d9d4e25342bf94b0766181f030db19e0f1e2cbf
kjssong <ssonggyu@gmail.com>
2019-01-02 23:36:16 +0900

    update

--- js/history.js
+++ js/history.js
@@ -9,13 +9,14 @@ $(document).ready(function(){
 
             var bNum = entry[i].title.$t,
             mName = entry[i].content.$t.match(/참가자이름: ([\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("참가자이름: ")[1];
-            /*pGame = entry[i].content.$t.match(/경기수: (\d+)/)[0].split("경기수: ")[1],
-            tPoint = entry[i].content.$t.match(/공격포인트: (\d+)/)[0].split("공격포인트: ")[1],
+            //pGame = entry[i].content.$t.match(/경기수: (\d+)/)[0].split("경기수: ")[1];
+
+
+           /* tPoint = entry[i].content.$t.match(/공격포인트: (\d+)/)[0].split("공격포인트: ")[1],
             myGoal = entry[i].content.$t.match(/골: (\d+)/)[0].split("골: ")[1],
             myAs = entry[i].content.$t.match(/도움: (\d+)/)[0].split("도움: ")[1];*/
-        
-        console.log(mName);
-        console.log(typeof mName);
+
+            $('.member__list').append('<li><span class="backnumber">' + bNum + '</span>'+ mName + '</li>')
         } 
 
     });

883f21db86e64b0e913636db81d10bff2748e069
kjssong <ssonggyu@gmail.com>
2019-01-04 18:04:28 +0900

    update

--- js/history.js
+++ js/history.js
@@ -1,41 +1,23 @@
 $(document).ready(function(){
-    var GSSurl = "https://spreadsheets.google.com/feeds/list/18j5b5XRHw_0k7xkO5BMfdN73zyGfnQSAfbeTkfMPDTg/1/public/basic?alt=json-in-script&callback=?";
-
     $.getJSON(GSSurl,function(data){
         var entry=data.feed.entry;
 
         for(var i in entry){
             var bNum = entry[i].title.$t,
-            mName = entry[i].content.$t.match(/참가자이름: ([\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("참가자이름: ")[1];
-            //pGame = entry[i].content.$t.match(/경기수: (\d+)/)[0].split("경기수: ")[1];
-            $('.member__list').append('<li><span class="backnumber">' + bNum + '</span>'+ mName + '</li>');
-        } 
-    });    
+            mName = entry[i].content.$t.match(/참가자이름: ([\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("참가자이름: ")[1],
+            pGameMem = entry[i].content.$t.match(/게임2: (\w+)/)[0].split("게임2: ")[1];
 
-    $.getJSON(GSSurl2,function(data){
-        var entry=data.feed.entry;
-        
-        for(var i in entry){
             
-            var sGame = entry[i].content.$t;
+            console.log(pGameMem);
 
-            console.log(sGame);
-           
-            //myTeam = entry[i].content.$t.match(/우리스코어: (\d+)/)[0].split("우리스코어: ")[1],
-            //youTeam = entry[i].content.$t.match(/상대스코어: (\d+)/)[0].split("상대스코어: ")[1],
-            //pName = entry[i].content.$t.match(/플레이멤버: ([\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("플레이멤버: ")[1];
             //pGame = entry[i].content.$t.match(/경기수: (\d+)/)[0].split("경기수: ")[1];
-
-            //$('.info__detail').append('<div class="set-box"><p class="txt__set">' + sGame + ' GAME</p><ul class="playmember__list"><li>' + sGame + '</li></ul></div>');           
+            
+            
+            $('.member__list').append('<li><span class="backnumber">'
+            + bNum
+            + '</span>'
+            + mName
+            + '</li>');
         } 
-        
-        
-        
-        
-
-        
-
     });
-
-
 });
---
+++ js/member.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    $('#btnBnum').on('click', function(){
+        var GSSurl = "https://spreadsheets.google.com/feeds/list/1L2bagzTWcQxts2YRzbtnL886-NpPoUZrNfxOLnv9bSI/1/public/basic?alt=json-in-script&orderby=column:번호&reverse=true&callback=?";
+    });
+    $('#btnMname').on('click', function(){
+        var GSSurl = "https://spreadsheets.google.com/feeds/list/1L2bagzTWcQxts2YRzbtnL886-NpPoUZrNfxOLnv9bSI/1/public/basic?alt=json-in-script&orderby=column:이름&callback=?";
+    });
+    $('#btnPgame').on('click', function(){
+        var GSSurl = "https://spreadsheets.google.com/feeds/list/1L2bagzTWcQxts2YRzbtnL886-NpPoUZrNfxOLnv9bSI/1/public/basic?alt=json-in-script&orderby=column:경기수&reverse=true&callback=?";
+    });
+    $('#btnPoint').on('click', function(){
+        var GSSurl = "https://spreadsheets.google.com/feeds/list/1L2bagzTWcQxts2YRzbtnL886-NpPoUZrNfxOLnv9bSI/1/public/basic?alt=json-in-script&orderby=column:공격포인트&reverse=true&callback=?";
+    });
+    $('#btnMygoal').on('click', function(){
+        var GSSurl = "https://spreadsheets.google.com/feeds/list/1L2bagzTWcQxts2YRzbtnL886-NpPoUZrNfxOLnv9bSI/1/public/basic?alt=json-in-script&orderby=column:골&reverse=true&callback=?";
+    });
+    $('#btnMyas').on('click', function(){
+        var GSSurl = "https://spreadsheets.google.com/feeds/list/1L2bagzTWcQxts2YRzbtnL886-NpPoUZrNfxOLnv9bSI/1/public/basic?alt=json-in-script&orderby=column:도움&reverse=true&callback=?";
+    });
+    
+    $.getJSON(GSSurl,function(data){
+        var entry=data.feed.entry;
+    
+        for(var i in entry){
+    
+            var eCon = entry[i].content.$t,
+            myRank = entry[i].title.$t,
+            bNum = eCon.match(/등번호: (\d+)/)[0].split("등번호: ")[1],
+            mName = eCon.match(/이름: ([\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("이름: ")[1],
+            rankScore = eCon.match(/파워랭킹: (\d+)/)[0].split("파워랭킹: ")[1],
+            pDay = eCon.match(/참석일수: (\d+)/)[0].split("참석일수: ")[1],
+            pGame = eCon.match(/경기수: (\d+)/)[0].split("경기수: ")[1],
+            gPoint = eCon.match(/승점: (\d+)/)[0].split("승점: ")[1],
+            myWin = eCon.match(/승: (\d+)/)[0].split("승: ")[1],
+            myDraw = eCon.match(/무: (\d+)/)[0].split("무: ")[1],
+            myLose = eCon.match(/패: (\d+)/)[0].split("패: ")[1],
+            winnerRate = eCon.match(/개인승률: (\d+)/)[0].split("개인승률: ")[1],
+            myGoal = eCon.match(/득점: (\d+)/)[0].split("득점: ")[1],
+            myAs = eCon.match(/도움: (\d+)/)[0].split("도움: ")[1],
+            mySave = eCon.match(/세이브: (\d+)/)[0].split("세이브: ")[1];
+
+            console.log(rankScore);
+
+            $(".member__list li:first-child").append("<li><p id='myRank' class='rank'>"
+            + myRank + "</p><p id='bNum' class='b-num'>"
+            + bNum + "</p><p id='mName' class='name'>"
+            + mName + "</p><p id='rankScore' class='rankscore'>"
+            + rankScore + "</p><p id='pDay' class='pday'>"
+            + pDay + "</p><p id='pGame' class='pgame'>"
+            + pGame + "</p><p id='gPoint' class='gpoint'>"
+            + gPoint + "</p><p id='myWin' class='win'>"
+            + myWin + "</p><p id='myDraw' class='draw'>"
+            + myDraw + "</p><p id='myLose' class='lose'>"
+            + myLose + "</p><p id='winnerRate' class='wr'>"
+            + winnerRate + "</p><p id='myGoal' class='mygoal'>"            
+            + myGoal + "</p><p id='myAs' class='myas'>"            
+            + myAs + "</p><p id='mySave' class='mysave'>"            
+            + mySave + "</p></li>");
+    
+            //console.log(entry[i].content.$t.match(/경기수: (\d+)/)[0].split("경기수: ")[1]);
+        } 
+    
+    });
+});

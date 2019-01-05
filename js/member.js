058860b6f0919f9d09e00739b3d950aad05e6a5d
kjssong <ssonggyu@gmail.com>
2019-01-05 13:17:38 +0900

    update

--- js/member.js
+++ js/member.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('#btnRank').on('click', function(){
-        var GSSurl = "https://spreadsheets.google.com/feeds/list/1L2bagzTWcQxts2YRzbtnL886-NpPoUZrNfxOLnv9bSI/1/public/basic?alt=json-in-script&orderby=column:순위&reverse=true&callback=?";
+        GSSurl = "https://spreadsheets.google.com/feeds/list/1L2bagzTWcQxts2YRzbtnL886-NpPoUZrNfxOLnv9bSI/1/public/basic?alt=json-in-script&orderby=column:순위&reverse=true&callback=?";
+
+        console.log('aaa');
     });
     
     $.getJSON(GSSurl,function(data){
@@ -23,8 +25,6 @@ $(document).ready(function(){
             myGoal = eCon.match(/득점: (\d+)/)[0].split("득점: ")[1],
             myAs = eCon.match(/도움: (\d+)/)[0].split("도움: ")[1],
             mySave = eCon.match(/세이브: (\d+)/)[0].split("세이브: ")[1];
-
-            console.log(rankScore);
 
             $(".member__list li:first-child").append("<li><p id='myRank' class='rank'>"
             + myRank + "</p><p id='bNum' class='b-num'>"

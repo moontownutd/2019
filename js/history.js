3e8146d2d2ea04312a19530a36433a790d145f78
kjssong <ssonggyu@gmail.com>
2019-01-17 22:02:13 +0900

    update

--- js/history.js
+++ js/history.js
@@ -1,23 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function(){   
+    setTimeout(function(){
+        $('.logo-box').remove();
+    },2000);
+
     $.getJSON(GSSurl,function(data){
         var entry=data.feed.entry;
-
-        for(var i in entry){
-            var bNum = entry[i].title.$t,
-            mName = entry[i].content.$t.match(/참가자이름: ([\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("참가자이름: ")[1],
-            pGameMem = entry[i].content.$t.match(/게임2: (\w+)/)[0].split("게임2: ")[1];
-
+    
+        for(var i in entry){    
+            var matchDate = entry[i].title.$t;
             
-            console.log(pGameMem);
-
-            //pGame = entry[i].content.$t.match(/경기수: (\d+)/)[0].split("경기수: ")[1];
-            
-            
-            $('.member__list').append('<li><span class="backnumber">'
-            + bNum
-            + '</span>'
-            + mName
-            + '</li>');
+            $(".match__list").append('<li><a href="' + matchDate + '/match.html">' + matchDate + '</a></li>');
         } 
+    
     });
 });

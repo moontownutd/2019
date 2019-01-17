0e96dd66329cb9bfde7d22cfc664190725c9123a
kjssong <ssonggyu@gmail.com>
2019-01-18 08:00:02 +0900

    update

--- js/history.js
+++ js/history.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
             eCon = entry[i].content.$t,
             youTeam = eCon.match(/상대팀: ([\D|\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("상대팀: ")[1];
             
-            $(".match__list").append('<li><a href="' + matchDate + '/match.html">' + matchDate + ' - vs <span>' + youTeam + '</span></a></li>');
+            $(".match__list").append('<li><a href="' + matchDate + '/match.html"><span id="num" class="num"></span><span id="titDate" class="tit__date">' + matchDate + '</span><span id="yTeam" class="yteam">' + youTeam + '</span></a></li>');
         } 
     
     });

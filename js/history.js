b08245b1cec07c2bbb2223e937a69befd353863b
kjssong <ssonggyu@gmail.com>
2019-01-18 21:40:38 +0900

    update

--- js/history.js
+++ js/history.js
@@ -7,11 +7,20 @@ $(document).ready(function(){
         var entry=data.feed.entry;
     
         for(var i in entry){    
-            var matchDate = entry[i].title.$t,
+            var matchNum = entry[i].title.$t,
             eCon = entry[i].content.$t,
-            youTeam = eCon.match(/상대팀: ([\D|\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("상대팀: ")[1];
+            matchDate = eCon.match(/날짜: (\d+)/)[0].split("날짜: ")[1],
+            youTeam = eCon.match(/상대팀: ([\w|\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("상대팀: ")[1].replace("_", " "),
+            myResult = eCon.match(/결과: ([?|\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("결과: ")[1],
+            goPlace = eCon.match(/장소: ([\w|\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]+)/g)[0].split("장소: ")[1];
             
-            $(".match__list").append('<li><a href="' + matchDate + '/match.html"><span id="num" class="num"></span><span id="titDate" class="tit__date">' + matchDate + '</span><span id="yTeam" class="yteam">' + youTeam + '</span></a></li>');
+            $(".match__list").append('<li><a href="'
+            + matchDate + '/match.html"><span id="num" class="num">'
+            + matchNum + '</span><span id="yTeam" class="yteam">'
+            + youTeam + '</span><span id="titDate" class="tit__date">'
+            + matchDate + '</span><span id="myResult" class="result">'
+            + myResult + '</span><span id="goPlace" class="place">'
+            + goPlace + '</span></a></li>');
         } 
     
     });
